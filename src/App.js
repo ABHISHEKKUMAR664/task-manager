35cd547365eb9a9057cb074c07824efd0c488126
Abhishek Kumar <7073921893a@gmail.com>
2021-08-08 13:37:22 +0530

    structure building

--- src/App.js
+++ src/App.js
@@ -3,6 +3,40 @@ function App() {
   return (
     <div className="App">
     <h1>Task Manager</h1>
+     
+     <form autoComplete="off">
+       <input type="text" id="taskmanger" name="taskmanger" 
+       required placeholder="what needs to be done?"></input>
+       <button type="submit">create</button>
+     </form>
+
+    <ul>
+      <li>
+        <label htmlFor="">
+        <input type="checkbox" id=""/>
+        Cooking task manger
+        </label>
+        <button>Edit</button>
+      </li>
+
+      <li>
+        <label htmlFor="">
+        <input type="checkbox" id=""/>
+        Cooking task manger
+        </label>
+        <button>Edit</button>
+      </li>
+    </ul>
+
+    <div className="row">
+      <label htmlFor="all">
+        <input type="checkbox" name="all" name="all" id="all"></input>
+        All
+      </label>
+      <p>You have 0 task to do</p>
+      <button id="delete">Delete</button>
+    </div>
+
     </div>
   );
 }
